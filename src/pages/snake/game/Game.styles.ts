608f59412e263137d52aa408c7787d9e3bb1cd90
clxrityy <clxrityxyz@gmail.com>
2023-02-13 17:41:49 -0500

    remove snake page :((

--- src/pages/snake/game/Game.styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const GameWrapper = styled.div`
-    width: 100%auto;
-    display: flex;
-    align-items: center;
-    outline: none;
-    flex-direction: column;
-`
