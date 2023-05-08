476dfa897b39b7127cb75ff1aa9095c2717fa0f3
clxrity <clxrityxyz@gmail.com>
2023-05-08 00:47:27 +0000

    we'll see

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        "../styled-components",
-        "../react-icons/ai",
-        "../react-icons/si",
-        "../react-icons/bs",
-        "../react-icons/im",
-        "@react-three/fiber",
-        "@react-three/drei",
-      ]
-    }
-  },
 })
