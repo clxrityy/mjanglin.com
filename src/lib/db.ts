85190433dc88a84ea64d64006d7c266c7cea357e
clxrityy <97744702+clxrityy@users.noreply.github.com>
2024-06-12 22:29:51 -0400

    change this

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,32 @@
 import { PrismaClient } from "@prisma/client";
-import { DynamicClientExtensionThis } from "@prisma/client/runtime/library";
 import { withAccelerate } from '@prisma/extension-accelerate'
 
-declare global {
-    var prisma: DynamicClientExtensionThis<PrismaClient, any, any> | undefined;
+// declare global {
+//     var prisma: DynamicClientExtensionThis<PrismaClient, any, any> | undefined;
+// }
+
+// export const db = globalThis.prisma || new PrismaClient().$extends(withAccelerate());
+
+// if (process.env.NODE_ENV !== "production") {
+//     globalThis.prisma = db;
+// }
+
+function makePrisma() {
+    return new PrismaClient({
+        datasources: {
+            db: {
+                url: process.env.ACCELERATE_URL!,
+            }
+        }
+    }).$extends(withAccelerate());
 }
 
-export const db = globalThis.prisma || new PrismaClient().$extends(withAccelerate());
+const globalForPrisma = global as unknown as {
+    prisma: ReturnType<typeof makePrisma>;
+}
+
+export const db = globalForPrisma.prisma ?? makePrisma();
 
 if (process.env.NODE_ENV !== "production") {
-    globalThis.prisma = db;
+    globalForPrisma.prisma = makePrisma();
 }
