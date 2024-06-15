33477bef4757cd89d36b786e8269e80612365264
clxrityy <97744702+clxrityy@users.noreply.github.com>
2024-06-15 14:39:19 -0400

    if this doesnt work im throwing up

--- src/handlers/birthday/view.ts
+++ src/handlers/birthday/view.ts
@@ -46,7 +46,7 @@ export default async function birthdayView(options: InteractionOption[], userId:
             }
         }
     } else {
-        const targetUser: string = JSON.parse(JSON.stringify(options.find((option) => option.name === "user")!.value));
+        const targetUser = options.find((option) => option.name === "user")?.value as string;
 
         if (!targetUser) {
             embed = EMBEDS.error;
