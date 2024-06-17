f3f9f4f70737c92fc78aa998346457f85b0b5d8b
clxrityy <97744702+clxrityy@users.noreply.github.com>
2024-06-17 19:00:59 -0400

    fix

--- src/data/util/functions/guild.ts
+++ src/data/util/functions/guild.ts
@@ -11,13 +11,18 @@ import { Guild } from "@/types/guild";
 export async function fetchGuild(guildId: string): Promise<Guild> {
     const apiUrl = CONFIG.URLS.DISCORD_API_BASE_URL + `/guilds/${guildId}`;
 
-    const response = await fetch(apiUrl, {
-        headers: {
-            Authorization: `Bot ${CONFIG.VALUES.BOT_TOKEN}`
-        }
-    });
-
-    const guild = JSON.parse(await response.json()) as Guild;
-
-    return guild;
+    try {
+        const response = await fetch(apiUrl, {
+            headers: {
+                Authorization: `Bot ${CONFIG.VALUES.BOT_TOKEN}`
+            }
+        });
+    
+        const guild = JSON.parse(await response.json()) as Guild;
+    
+        return guild;
+    } catch (e: any) {
+        console.error(e);
+        throw new Error("Failed to fetch guild");
+    }
 }
--- src/handlers/general/index.ts
+++ src/handlers/general/index.ts
@@ -1,4 +1,4 @@
-import { astrologySignHandler } from "..";
+import astrologySignHandler from "./astrology/sign";
 import birthdayCountdown from "./birthday/countdown";
 import birthdaySet from "./birthday/set";
 import birthdayView from "./birthday/view";
