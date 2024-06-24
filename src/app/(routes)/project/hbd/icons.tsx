1ed472ca9d666efb9010b8af2e548456c0af2e5b
clxrityy <97744702+clxrityy@users.noreply.github.com>
2024-06-24 13:09:03 -0400

    new hbd blog

---
+++ src/app/(routes)/project/hbd/icons.tsx
@@ -0,0 +1,10 @@
+import { RiSlashCommands2 } from "react-icons/ri";
+import { LuLayoutTemplate } from "react-icons/lu";
+
+export function SlashCommandIcon() {
+    return <RiSlashCommands2 />
+}
+
+export function TemplateIcon() {
+    return <LuLayoutTemplate />
+}
