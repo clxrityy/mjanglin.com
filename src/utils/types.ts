56fea38f32aaa41d01eb1495dc59d7e9c6c49b19
clxrityy <97744702+clxrityy@users.noreply.github.com>
2024-06-30 19:12:46 -0400

    clean up

--- src/utils/types.ts
+++
@@ -1,9 +0,0 @@
-export type ProjectParams = {
-    name: string;
-    description: string;
-    link: string;
-    github?: string;
-    image: string;
-    footer?: string;
-    tags?: string[];
-}
