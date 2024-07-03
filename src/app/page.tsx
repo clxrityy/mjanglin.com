dd28ac02c17d12bba797ffced9e6631051dba88c
clxrityy <97744702+clxrityy@users.noreply.github.com>
2024-07-03 13:41:24 -0400

    and maybe fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import Button from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { useUser } from "@clerk/nextjs";
 import {ICONS} from "@/config/data/constants";
--- src/components/NewRecord.tsx
+++ src/components/NewRecord.tsx
@@ -4,7 +4,7 @@ import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { SleepUser } from "@prisma/client";
-import Button from "@/components/ui/button";
+import {Button} from "@/components/ui/button";
 import {
     Form,
     FormControl,
