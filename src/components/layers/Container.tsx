08e019f4a9dfe9755e6bcf5a417f68d6fa4d1bd4
clxrityy <97744702+clxrityy@users.noreply.github.com>
2024-07-22 13:56:18 -0400

    compress gifs

--- src/components/layers/Container.tsx
+++ src/components/layers/Container.tsx
@@ -10,7 +10,7 @@ type Props = {
 export default async function ProjectsContainer({ projects, ...props }: Props) {
 
     return <div {...props} className="w-fit h-fit px-10 py-5 rounded-md">
-        <div className="flex flex-col gap-6">
+        <div className="flex items-center justify-center w-full gap-6">
             <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5 justify-stretch">
                 {projects.map((project, index) => (
                     <Suspense key={index} fallback={<Skeleton />}>
