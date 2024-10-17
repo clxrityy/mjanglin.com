3c622cb369134d68604037bf6e2f1a1cc6b55e8e
clxrityy <clxrityxyz@gmail.com>
2024-10-17 07:28:37 -0700

    fix formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,25 @@
 "use client"
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { useUser } from "@clerk/nextjs";
 import { ICONS } from "@/config/data/constants";
 import BigButton from "@/components/ui/bigButton";
+import { useRouter } from "next/navigation";
 
 
 export default function Home() {
 
   const { user, isLoaded, isSignedIn } = useUser();
+  const router = useRouter();
+
+  if (!isLoaded) {
+    router.refresh();
+  }
 
   if (isLoaded) {
 
     return (
       <main>
-        <div className="w-full h-full flex flex-col items-center justify-center gap-10">
+        <div className="w-full h-screen flex flex-col items-center justify-center gap-20 my-20">
           <div className="flex flex-col my-10 items-center">
             <div className="flex flex-col items-center gap-5">
               <h1 className="text-5xl font-bold uppercase">sleep graph</h1>
@@ -44,8 +49,10 @@ export default function Home() {
               }
             </div>
           </div>
-          <div className="clock-container">
-            <div className="clock" />
+          <div className="flex items-center justify-center w-full h-full py-10 relative">
+            <div className="clock-container">
+              <div className="clock" />
+            </div>
           </div>
         </div>
       </main>
