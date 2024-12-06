30e70f30d7a4551a97662ffa239c82a8cade0137
clxrityy <clxrityxyz@gmail.com>
2024-12-06 18:02:20 -0500

    SEO best practices

--- src/components/layout/SocialsMenu.tsx
+++ src/components/layout/SocialsMenu.tsx
@@ -7,23 +7,23 @@ export function SocialsMenu() {
     return (
         /* From Uiverse.io by akshat-patel28 */
         <div className="socials-button-container z-50">
-            <button className="socials-button">
-                <Link href={URLS.spotify} className="text-inherit hover:text-inherit">
+            <button className="socials-button" aria-label="spotify">
+                <Link href={URLS.spotify} className="text-inherit hover:text-inherit" aria-label="spotify">
                     <FaSpotify className="socials-icon" />
                 </Link>
             </button>
-            <button className="socials-button">
-                <Link href={URLS.github} className="text-inherit hover:text-inherit">
+            <button className="socials-button" aria-label="github">
+                <Link href={URLS.github} className="text-inherit hover:text-inherit" aria-label="github">
                     <FaGithub className="socials-icon" />
                 </Link>
             </button>
-            <button className="socials-button">
-                <Link href={URLS.instagram} className="text-inherit hover:text-inherit">
+            <button className="socials-button" aria-label="instagram">
+                <Link href={URLS.instagram} className="text-inherit hover:text-inherit" aria-label="instagram">
                     <FaInstagram className="socials-icon" />
                 </Link>
             </button>
-            <button className="socials-button">
-                <Link href={URLS.discord} className="text-inherit hover:text-inherit">
+            <button className="socials-button" aria-label="discord">
+                <Link href={URLS.discord} className="text-inherit hover:text-inherit" aria-label="discord">
                     <FaDiscord className="socials-icon" />
                 </Link>
             </button>
