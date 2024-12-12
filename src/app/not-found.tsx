622da16c5434979ae1eef8f0028d870372eff356
clxrityy <clxrityxyz@gmail.com>
2024-12-12 01:10:34 -0500

    custom 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,32 @@
+import "@/styles/404.css";
+
+export default function Error() {
+    return (
+        /* From Uiverse.io by Subaashbala */
+        <div className="not-found font-exo">
+            <h1>
+                404
+            </h1>
+            <p>
+                Page not found
+            </p>
+            <div className="modelViewPort">
+                <div className="eva">
+                    <div className="head">
+                        <div className="eyeChamber">
+                            <div className="eye"></div>
+                            <div className="eye"></div>
+                        </div>
+                    </div>
+                    <div className="body">
+                        <div className="hand"></div>
+                        <div className="hand"></div>
+                        <div className="scannerThing"></div>
+                        <div className="scannerOrigin"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
