4820fc805ccedc8e4bc279b32bb9d27ae26aafc4
clxrityy <clxrityxyz@gmail.com>
2025-02-06 03:27:22 -0500

    fixcks

--- src/app/(main)/(routes)/posts/page.tsx
+++ src/app/(main)/(routes)/posts/page.tsx
@@ -7,9 +7,9 @@ export default async function Page() {
     const posts = await getAllPosts();
 
     return (
-        <div className="h-full overflow-y-scroll scroll-smooth mx-auto relative">
-            <div className="flex flex-col items-center justify-center gap-10 lg:gap-12 xl:gap-16 mt-96 sm:mt-80 md:mt-72 lg:mt-0 xl:-mt-20 h-full">
-                <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-8 xl:gap-10 2xl:gap-12 items-center justify-between h-auto">
+        <div className="h-full mx-auto relative w-max xl:mt-10">
+            <div className="flex flex-col items-center justify-center gap-10 lg:gap-12 xl:gap-16 mt-96 sm:mt-80 md:mt-72 lg:mt-0 h-full">
+                <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-8 xl:gap-10 2xl:gap-12 items-center justify-between h-auto w-full mx-auto">
                     {posts.map((post, idx) => (
                         <PostCard key={idx} post={post} />
                     ))}
