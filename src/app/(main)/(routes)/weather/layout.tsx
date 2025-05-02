98926863b8b8144a916caa0ae8a5bb85db24cf9d
clxrityy <clxrityxyz@gmail.com>
2025-05-02 14:36:00 -0400

    await connection

--- src/app/(main)/(routes)/weather/layout.tsx
+++ src/app/(main)/(routes)/weather/layout.tsx
@@ -2,6 +2,7 @@ import { fetchIp, getLocationByIp } from "@/utils/ip";
 import "@/styles/css/weather.css";
 import Link from "next/link";
 import { MdArrowBack } from "react-icons/md";
+import { connection } from "next/server";
 
 async function getStaticProps() {
     const ip = await fetchIp() as string;
@@ -26,6 +27,7 @@ export async function generateMetadata() {
 
 export default async function Layout({children}: {children: React.ReactNode}) {
 
+    await connection();
 
     return (
         <div className="w-screen h-screen relative">
