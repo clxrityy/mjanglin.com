c016d135f18a555f71b9b2d42430d179dddfd44b
clxrityy <clxrityxyz@gmail.com>
2025-05-10 21:29:35 -0400

    adjust back button

--- src/app/(main)/(routes)/posts/layout.tsx
+++ src/app/(main)/(routes)/posts/layout.tsx
@@ -1,6 +1,7 @@
+import { BackBtn } from "@/components/buttons/BackBtn";
 import "@/styles/css/post.css";
 import { Metadata } from "next";
-import Link from "next/link";
+import { Suspense } from "react";
 import { MdArrowBack } from "react-icons/md";
 
 export const metadata: Metadata = {
@@ -12,9 +13,9 @@ export const metadata: Metadata = {
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div className="relative min-h-screen w-full mx-auto">
-            <Link href="/posts" className="fixed top-0 left-0 px-3 py-4 link" aria-label="back">
-                <MdArrowBack size={30} className="relative z-50" />
-            </Link>
+            <Suspense fallback={<MdArrowBack size={30} className="animate-pulse text-white/20" />}>
+                <BackBtn />
+            </Suspense>
             <div className="flex items-center justify-center w-full">
                 <div className="w-full lg:w-2/3 xl:w-1/2">
                     <div className="py-10 markdown-body">
