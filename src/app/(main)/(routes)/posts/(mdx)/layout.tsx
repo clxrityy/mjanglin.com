c06545dd85d96fbe1e83ab0e16b21588e79058c5
clxrityy <clxrityxyz@gmail.com>
2025-07-11 19:58:46 -0400

    update posts layout(s)

---
+++ src/app/(main)/(routes)/posts/(mdx)/layout.tsx
@@ -0,0 +1,12 @@
+import "@/styles/css/post.css";
+import { ReactNode } from "react";
+
+export default function Layout({ children }: { children: ReactNode }) {
+    return (
+        <div className="w-full lg:w-2/3 xl:w-1/2">
+            <div className="markdown-body">
+                {children}
+            </div>
+        </div>
+    )
+}
--- src/app/(main)/(routes)/posts/layout.tsx
+++ src/app/(main)/(routes)/posts/layout.tsx
@@ -1,5 +1,4 @@
 import { BackBtn } from "@/components/buttons/BackBtn";
-import "@/styles/css/post.css";
 import { Metadata } from "next";
 import { Suspense } from "react";
 import { MdArrowBack } from "react-icons/md";
@@ -16,12 +15,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             <Suspense fallback={<MdArrowBack size={30} className="animate-pulse text-white/20" />}>
                 <BackBtn />
             </Suspense>
-            <div className="flex items-center justify-center w-full">
-                <div className="w-full lg:w-2/3 xl:w-1/2">
-                    <div className="py-10 markdown-body">
-                        {children}
-                    </div>
-                </div>
+            <div className="flex items-center justify-center w-full overflow-y-scroll py-10">
+                {children}
             </div>
         </div>
     )
