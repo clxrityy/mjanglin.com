289a1c995cbbd13808a55c3d2ffdde9106f48312
clxrityy <clxrityxyz@gmail.com>
2025-09-29 11:35:40 -0400

    move some utils -> lib

--- src/lib/ip.ts
+++ src/lib/ip.ts
@@ -1,15 +1,15 @@
 import axios from 'axios';
 import { URLS } from '../config';
-import { IpGeolocation } from './types';
+import { IpGeolocation } from '../utils/types';
 
 
 export async function fetchIp(): Promise<string> {
-    const { ip } = await axios.get<{ip: string}>(`${URLS.API.ipify}?format=json`).then(res => res.data);
+    const { ip } = await axios.get<{ ip: string }>(`${URLS.API.ipify}?format=json`).then(res => res.data);
 
     return ip;
 }
 
-export async function getLocationByIp(ip: string): Promise<IpGeolocation | undefined>{
+export async function getLocationByIp(ip: string): Promise<IpGeolocation | undefined> {
     try {
         const { query, status, country, countryCode, region, regionName, city, zip, lat, lon, timezone, isp, org, as } = await axios.get<IpGeolocation>(`${URLS.API.ip_api}${ip}`).then(res => res.data);
 
