f705c51164803d9d136edbf6bc4c845462c528ca
clxrityy <97744702+clxrityy@users.noreply.github.com>
2026-08-30 13:37:37 -0400

    use back btn

--- src/app/(main)/(routes)/layout.tsx
+++ src/app/(main)/(routes)/layout.tsx
@@ -1,18 +1,11 @@
-import Link from "next/link";
-import { MdArrowBack } from "react-icons/md";
+import { BackBtn } from "@/components/buttons/BackBtn";
 
 export default async function Layout({
 	children,
 }: Readonly<{ children: React.ReactNode }>) {
 	return (
 		<>
-			<Link
-				href="/"
-				className="absolute top-0 px-3 py-4 link"
-				aria-label="back"
-			>
-				<MdArrowBack size={30} className="relative z-50" />
-			</Link>
+			<BackBtn />
 			{children}
 		</>
 	);
