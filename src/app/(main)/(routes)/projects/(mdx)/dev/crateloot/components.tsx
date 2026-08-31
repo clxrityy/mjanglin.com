e25ccda34d10bd41c808439b8cadf597d5dc9a52
clxrityy <97744702+clxrityy@users.noreply.github.com>
2026-08-31 13:46:49 -0400

    bottom padding

--- src/app/(main)/(routes)/projects/(mdx)/dev/crateloot/components.tsx
+++ src/app/(main)/(routes)/projects/(mdx)/dev/crateloot/components.tsx
@@ -206,7 +206,7 @@ export function CrateLootCommands() {
 
 export function CrateLootDownload() {
 	return (
-		<div className="flex flex-col items-center justify-center gap-2 w-full">
+		<div className="flex flex-col items-center justify-center gap-2 w-full pb-6">
 			<Link href={r2AssetPath("assets/CrateLoot-0.4.0.jar")} download>
 				<button type="button" className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-colors duration-300">
 					CrateLoot-0.4.0.jar
